42cabc60725be356203a73e83c3fd29586dce9a1
vcheb <slavacheban22@gmail.com>
2025-08-25 22:01:43 +0300

    updated start sn-user with docker compose

--- sn-user/src/main/java/com/sn/snuser/model/embeddable/Contacts.java
+++ sn-user/src/main/java/com/sn/snuser/model/embeddable/Contacts.java
@@ -17,15 +17,18 @@ public class Contacts {
     @ElementCollection
     @CollectionTable(name = "user_mobiles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "mobile")
+    @Builder.Default
     private List<String> mobiles = new ArrayList<>();
 
     @ElementCollection
     @CollectionTable(name = "user_emails", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "email")
+    @Builder.Default
     private List<String> emails = new ArrayList<>();
 
     @ElementCollection
     @CollectionTable(name = "user_sites", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "site")
+    @Builder.Default
     private List<String> sites = new ArrayList<>();
 }
