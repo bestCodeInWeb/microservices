1ff0fa20d957ec0018f2ce539ed1a833cdee12a9
vcheb <slavacheban22@gmail.com>
2025-08-30 21:55:33 +0300

    added test end point

--- sn-user/src/main/java/com/sn/snuser/config/SecurityConfig.java
+++ sn-user/src/main/java/com/sn/snuser/config/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.sn.snuser.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.HttpMethod.OPTIONS;
 
 @Configuration
 public class SecurityConfig {
@@ -15,11 +19,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(OPTIONS).permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
 
                 )
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
@@ -31,6 +37,8 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")           // все пути
                         .allowedOrigins("http://localhost:5173") // откуда разрешаем
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
